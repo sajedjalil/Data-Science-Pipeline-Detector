ef0a72739afb4765944c87b79c7bc267157e63b5
sajedjalil <sjalil@gmu.edu>
2022-07-21 00:30:01 -0400

    Changed path in unittest
    Signed-off-by: sajedjalil <sjalil@gmu.edu>

--- src/test_ipynb_pipeline_detector.py
+++ src/test_ipynb_pipeline_detector.py
@@ -8,7 +8,7 @@ class TestIpynbPipelineDetector(TestCase):
 
     def test_get_ast_notebook_file(self):
         api_dict_df = read_xlsx(os.path.join(res_folder_path, api_dict_file))
-        path = "/home/sajed/GitHub/Data-Science-Pipeline-Detector/dataset/house-prices-advanced-regression-techniques" \
-               "/Pedro Marcelino, PhD/comprehensive-data-exploration-with-python.ipynb"
+        path = dataset_base_path + "/house-prices-advanced-regression-techniques/Pedro Marcelino, " \
+                                   "PhD/comprehensive-data-exploration-with-python.ipynb"
         result_nodes = get_ast_notebook_file(api_dict_df, path)
         self.assertEqual(len(result_nodes), 50)
