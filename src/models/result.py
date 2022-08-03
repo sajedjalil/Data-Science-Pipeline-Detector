745611a7b09f398ee0bf73cb85174a1d78f0b75e
Sajed Jalil <sjalil@gmu.edu>
2022-08-03 18:34:46 -0400

    Reduced bugs in parameter detection
    Signed-off-by: Sajed Jalil <sjalil@gmu.edu>

--- src/models/result.py
+++ src/models/result.py
@@ -6,10 +6,18 @@ class Result:
     column_no: int = None
     parameters: list = None
 
-    def __init__(self, pipeline, keyword, node, cell_no):
-        self.keyword = keyword
-        self.pipeline_step = pipeline
-        self.column_no = int(node.end_col_offset)
-        self.cell_no = cell_no
-        self.line_no = node.lineno
-        self.parameters = []
+    def __init__(self, *args):
+        if isinstance(args[0], str):
+            self.pipeline_step = args[0]
+            self.keyword = args[1]
+            self.column_no = int(args[2].end_col_offset)
+            self.line_no = args[2].lineno
+            self.cell_no = args[3]
+            self.parameters = []
+        else:
+            self.keyword = args[0].keyword
+            self.pipeline_step = args[0].pipeline_step
+            self.column_no = args[0].column_no
+            self.cell_no = args[0].cell_no
+            self.line_no = args[0].line_no
+            self.parameters = args[0].parameters
--- src/pipeline/ast_analyzer.py
+++ src/pipeline/ast_analyzer.py
@@ -1,11 +1,13 @@
 import ast
 from src.models.result import Result
-from pprint import pprint
-import copy
 
 
 def is_constructor(node):
     return isinstance(node, ast.Call) and isinstance(node.func, ast.Name)
+
+
+def is_attribute(node):
+    return isinstance(node, ast.Call) and isinstance(node.args, list)
 
 
 def get_tuple_name(node) -> list:
@@ -30,23 +32,14 @@ class Analyzer(ast.NodeVisitor):
 
     def visit_Attribute(self, node):
         response = self.make_result_node(node)
-
         if response:
-            self.temp_result_node = response
+            self.temp_result_node = Result(response)
             self.generic_visit(node)
             self.result_nodes.append(self.temp_result_node)
         else:
             self.generic_visit(node)
 
-    def visit_Name(self, node):
-        # if self.temp_result_node:
-        #     pprint(ast.dump(node))
-        #     print(self.temp_result_node)
-        #     # self.temp_result_node.parameters.append(node.id)
-        self.generic_visit(node)
-
     def visit_Constant(self, node):
-
         if self.temp_result_node:
             self.temp_result_node.parameters.append(node.value)
         self.generic_visit(node)
@@ -55,28 +48,40 @@ class Analyzer(ast.NodeVisitor):
         if is_constructor(node):
             response = self.make_result_node(node)
             if response is not None:
-                self.temp_result_node = response
+                # pprint(ast.dump(node))
+                self.temp_result_node = Result(response)
                 self.generic_visit(node)
+                self.get_named_arguments(node.args)
                 self.result_nodes.append(self.temp_result_node)
-                self.temp_result_node = None
         else:
             self.generic_visit(node)
+            if is_attribute(node):
+                # pprint(ast.dump(node))
+                self.get_named_arguments(node.args)
 
     def visit_keyword(self, node):
         if self.temp_result_node:
-            if isinstance(node.value, ast.Constant):
-                # print(node.value.value, node.arg)
+            if hasattr(node, 'value') and isinstance(node.value, ast.Constant):
                 self.temp_result_node.parameters.append([node.arg, node.value.value])
-            elif isinstance(node.value, ast.Tuple):
-                # print(node.value.value, node.arg)
-                # self.visit_Tuple(node.value)
+            elif hasattr(node, 'value') and isinstance(node.value, ast.Tuple):
                 self.temp_result_node.parameters.append([node.arg, get_tuple_name(node.value)])
-            elif isinstance(node.value, ast.Name):
-                print(node.value.id)
+            elif hasattr(node, 'id') and isinstance(node, ast.Name):
+                self.temp_result_node.parameters.append(node.id)
 
-    # def visit_arg(self, node):
-    #     pprint( ast.dump(node))
-    #     self.generic_visit(node)
+    def get_named_arguments(self, nodes):
+        if not self.temp_result_node:
+            return
+        params = []
+        for node in nodes:
+            if hasattr(node, 'value') and isinstance(node.value, ast.Constant):
+                params.append([node.arg, node.value.value])
+            elif hasattr(node, 'value') and isinstance(node.value, ast.Tuple):
+                params.append([node.arg, get_tuple_name(node.value)])
+            elif hasattr(node, 'id') and isinstance(node, ast.Name):
+                params.append(node.id)
+
+        if len(params):
+            self.temp_result_node.parameters.extend(params)
 
     def generic_visit(self, node):
         ast.NodeVisitor.generic_visit(self, node)
@@ -89,11 +94,11 @@ class Analyzer(ast.NodeVisitor):
         elif isinstance(node, ast.Attribute):
             value = node.attr
 
-        for col in self.api_dict_df.columns:
-            is_found = self.api_dict_df[col].isin([value])
-            keyword = self.api_dict_df[col].values[is_found]
+        for pipeline in self.api_dict_df.columns:
+            is_found = self.api_dict_df[pipeline].isin([value])
+            keyword = self.api_dict_df[pipeline].values[is_found]
 
             if len(keyword) > 0:
-                return Result(col, keyword, node, self.cell_no)
+                return Result(pipeline, keyword, node, self.cell_no)
 
         return None
