82d46abdac689f6fda89d8c92dafbb40f97a6b6c
Sajed Jalil <sjalil@gmu.edu>
2022-09-06 05:52:29 -0400

    More Frequency Metrics are Added
    Signed-off-by: Sajed Jalil <sjalil@gmu.edu>

--- src/tests/test_ast_frequency_analyzer.py
+++ src/tests/test_ast_frequency_analyzer.py
@@ -1,9 +1,8 @@
-from pprint import pprint
+from collections import OrderedDict
 from unittest import TestCase
 from src.constants.constants import *
 from src.ast.ast_parser import *
 from src.utils.file_reader import read_xlsx, NotebookReader
-from src.models.result import Result
 
 
 class TestAnalyzer(TestCase):
@@ -41,5 +40,52 @@ class TestAnalyzer(TestCase):
     #
     #     for pair in threshold_map:
     #         print(pair, threshold_map[pair])
+    #
+    #     self.assertEqual(len(all_ipynb_paths), 14498)
+
+    # def test_count_keyword_per_class_modeling_step(self):
+    #     api_dict_df = read_xlsx(os.path.join(os.getcwd(), res_folder, api_dict_file))
+    #     all_ipynb_paths = NotebookReader(os.path.join(os.getcwd(), dataset_folder)).all_ipynb_paths
+    #
+    #     threshold_map = {}
+    #     for keyword in api_dict_df['Modeling']:
+    #         threshold_map[keyword] = 0
+    #
+    #     for idx, path in enumerate(all_ipynb_paths):
+    #         parser = Parser(api_dict_df, path)
+    #         results = parser.ast_parse_frequency()
+    #
+    #         keyword_set = set()
+    #         for res in results:
+    #             if res.pipeline_step == "Modeling":
+    #                 keyword_set.add(res.keyword[0])
+    #         print(idx, path)
+    #
+    #         for item in keyword_set:
+    #             threshold_map[item] += 1
+    #
+    #     for pair in threshold_map:
+    #         print(pair, threshold_map[pair])
+    #
+    #     self.assertEqual(len(all_ipynb_paths), 14498)
+
+    # def test_pipeline_step_count_per_class(self):
+    #     api_dict_df = read_xlsx(os.path.join(os.getcwd(), res_folder, api_dict_file))
+    #     all_ipynb_paths = NotebookReader(os.path.join(os.getcwd(), dataset_folder)).all_ipynb_paths
+    #
+    #     threshold_map = {}
+    #
+    #     for idx, path in enumerate(all_ipynb_paths):
+    #         parser = Parser(api_dict_df, path)
+    #         key = len(parser.ast_parse_frequency())
+    #
+    #         if key not in threshold_map.keys():
+    #             threshold_map[key] = 0
+    #         threshold_map[key] += 1
+    #
+    #     od = OrderedDict(sorted(threshold_map.items()))
+    #
+    #     for key in od:
+    #         print(key, od[key])
     #
     #     self.assertEqual(len(all_ipynb_paths), 14498)
